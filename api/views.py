76d425469ad1444a809a526f35df77524e3e5f87
Polina <forstudents552@gmail.com>
2026-04-01 04:31:35 +0400

    дай бог правильно

---
+++ api/views.py
@@ -0,0 +1,35 @@
+from .serializers import GroupSerializer,PostSerializer,CommentSerializer
+from posts.models import Group, Post, Comment
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsAuthorOrReadOnly
+# Create your views here.
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (IsAuthenticated,)
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+        
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticated, IsAuthorOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post = self.get_post())
+
+    def get_post(self):
+        return get_object_or_404(Post, pk = self.kwargs.get('post_id'))
+    
+    def get_queryset(self):
+        post = self.get_post()
+        return post.comments.all()
